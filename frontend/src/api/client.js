39b9d064704ba6ab774aad973467da7c6146dacf
Guptasamarpit <samarpitgupta630@gmail.com>
2026-09-01 14:19:16 +0530

    Configure production CORS

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const api = axios.create({ baseURL: "http://localhost:8080/api" });
+// export const api = axios.create({ baseURL: "http://localhost:8080/api" });
+export const api = axios.create({ baseURL: "https://taskflow-backend-ipd1.onrender.com/api" });
 api.interceptors.request.use((c) => {
   const t = localStorage.getItem("token");
   if (t) c.headers.Authorization = `Bearer ${t}`;
